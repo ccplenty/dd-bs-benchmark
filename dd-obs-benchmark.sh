8a20aaf8968a3e9b2b2dcec2b1a4b07c0aaae078
Alin Pasol <programming.alinpasol@gmx.com>
2019-01-03 23:18:16 +0200

    Add checks and tests to dd-obs-benchmark.sh
    Add security checks and remove the temporary file in case the script
    exits prematurely.

--- dd-obs-benchmark.sh
+++ dd-obs-benchmark.sh
@@ -5,7 +5,13 @@ set -e
 set -u
 #set -x
 
-path="${1}"
+if [[ "${1:-}" ]]; then
+  path="${1}"
+else
+  echo "Please provide a directory path!"
+  echo "Usage: ${0##*/} -r|-w DIRECTORY [NUMBER]"
+  exit 1
+fi
 temporary_file="$path/dd-ibs-benchmark.tmp"
 if [[ "${2:-}" ]]; then temporary_file_size="${2}"; else temporary_file_size=268435456; fi
 block_sizes="512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608 16777216 33554432 67108864"
@@ -15,26 +21,40 @@ if [[ $EUID -ne 0 ]]; then
   echo "To avoid inaccurate results please run this script as root." >&2
 fi
 
+if [[ ! -d $path || ! -w $path ]]; then
+  echo "$path is not a directory or is not writable, please provide another path."
+  exit 1
+fi
+
 if [[ -e $temporary_file ]]; then
   echo "The file $temporary_file exists, please provide another file path."
   exit 1
 fi
 
-echo "block  |  write"
-echo " size  |  speed"
+case "$temporary_file_size" in
+  (*[!0-9]*|'') echo "Please specify a file size that is a natural number/positive integer!";;
+  (*)           echo "The file will be $temporary_file_size bytes large.";;
+esac
 
-for bs in $block_sizes; do
+{
+  echo "block  |  write"
+  echo " size  |  speed"
 
-  count=$((temporary_file_size / bs))
+  for bs in $block_sizes; do
 
-  sync && [[ $EUID -eq 0 ]] && [[ -e /proc/sys/vm/drop_caches ]] && sysctl vm.drop_caches=3
+    count=$((temporary_file_size / bs))
 
-  dd_output=$(dd bs="$bs" conv=fsync count=$count if=/dev/zero of="$temporary_file" 2>&1 1>/dev/null)
+    sync && [[ $EUID -eq 0 ]] && [[ -e /proc/sys/vm/drop_caches ]] && sysctl vm.drop_caches=3
 
-  write_speed=$(grep --only-matching --extended-regexp --ignore-case '[0-9.]+ ([GMk]?B|bytes)/s(ec)?' <<< "$dd_output")
+    dd_output=$(dd bs="$bs" conv=fsync count=$count if=/dev/zero of="$temporary_file" 2>&1 1>/dev/null)
 
-  echo "  $bs  -  $write_speed"
+    write_speed=$(grep --only-matching --extended-regexp --ignore-case '[0-9.]+ ([GMk]?B|bytes)/s(ec)?' <<< "$dd_output")
 
+    echo "  $bs  -  $write_speed"
+
+    rm "$temporary_file"
+
+  done
+} || {
   rm "$temporary_file"
-
-done
+}
